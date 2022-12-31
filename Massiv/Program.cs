62f2dd9169ee2016fd1ccaef2918f68e3fdd9ee7
Сергей Зуев <s.zuev@MacBook-Pro-Sergej.local>
2022-12-31 15:12:07 +0300

    Написание программы

--- Massiv/Program.cs
+++ Massiv/Program.cs
@@ -5,3 +5,45 @@
 исключительно массивами.
 */
 
+void Print(string[] arr)
+{
+    int size = arr.Length;
+
+    for (int i = 0; i < size; i++)
+    {   
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
+}
+
+string[] Massiv(int size)
+{
+    string[] arr = new string[size];
+    Console.WriteLine("Введите элементы массива: ");
+
+    for (int i = 0; i < size; i++)
+    {  
+        arr[i] = Console.ReadLine();    
+    }
+    return arr;
+}
+
+string[] NewMassiv(string[] arr)
+{
+    string newmassiv = "";
+    foreach (string elem in arr)
+    {
+        if (elem.Length <= 3)
+            newmassiv += elem + " ";        
+    }  
+    string[] NewArr = newmassiv.Split(' ');
+    return NewArr;      
+}
+
+Console.WriteLine("Введите длину массива: ");
+string[] arr_1 = Massiv(int.Parse(Console.ReadLine()));
+Console.WriteLine("Исходный массив: ");
+Print(arr_1);
+string[] new_arr = NewMassiv(arr_1);
+Console.WriteLine("Полученный массив: ");
+Print(new_arr);
